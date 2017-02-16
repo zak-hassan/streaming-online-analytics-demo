7ae3869401233e431c653b33f0d7c675950f8ad5
Zak Hassan <zak.hassan1010@gmail.com>
2017-02-16 09:53:32 -0500

    Adding kafka streaming to spark app.

--- spark-streaming-service/src/main/java/com/example/spark/App.java
+++ spark-streaming-service/src/main/java/com/example/spark/App.java
@@ -1,49 +1,85 @@
 package com.example.spark;
 
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.streaming.StreamingQuery;
-import org.apache.spark.sql.streaming.StreamingQueryException;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaPairReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka.KafkaUtils;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.regex.Pattern;
 
-public class App   {
+import scala.Tuple2;
+
+public class App {
+
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    public static void main(String[] args) throws Exception {
+        // Setting up kafka
+
+        Map<String, Object> kafkaParams = new HashMap<>();
+        kafkaParams.put("bootstrap.servers", "localhost:9092");
+        kafkaParams.put("key.deserializer", StringDeserializer.class);
+        kafkaParams.put("value.deserializer", StringDeserializer.class);
+        kafkaParams.put("group.id", "use_a_separate_group_id_for_each_stream");
+        kafkaParams.put("auto.offset.reset", "latest");
+        kafkaParams.put("enable.auto.commit", false);
+        Map<String, Integer> topicMap = new HashMap<>();
+
+        topicMap.put("test", 2);
+        topicMap.put("zak", 2);
+//        Collection<String> topics = Arrays.asList("test", "zak");
 
 
-	public static void main(String[] args) throws StreamingQueryException {
-        SparkSession spark = SparkSession
-                .builder().master("local[*]")
-                .appName("OrderPipeline")
-                .getOrCreate();
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("orderPurchaseService");
 
-         Dataset<Row> lines = spark
-                .readStream()
-                .format("socket")
-                .option("host", "localhost")
-                .option("port", 9999)
-                .load();
+        JavaStreamingContext streamingContext = new JavaStreamingContext(conf, Durations.seconds(1));
 
-         Dataset<String> words = lines
-                .as(Encoders.STRING())
-                .flatMap(
-                        new FlatMapFunction<String, String>() {
-                            @Override
-                            public Iterator<String> call(String x) {
-                                return Arrays.asList(x.split(" ")).iterator();
-                            }
-                        }, Encoders.STRING());
 
-         Dataset<Row> wordCounts = words.groupBy("value").count();
-         StreamingQuery query = wordCounts.writeStream()
-                .outputMode("complete")
-                .format("console")
-                .start();
+        JavaPairReceiverInputDStream<String, String> messages = KafkaUtils.createStream(streamingContext, "localhost:2181", "1", topicMap);
 
-        query.awaitTermination();
 
-	}
+        JavaDStream<String> lines = messages.map(new Function<Tuple2<String, String>, String>() {
+            @Override
+            public String call(Tuple2<String, String> tuple2) {
+                return tuple2._2();
+            }
+        });
+        JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public Iterator<String> call(String x) {
+                return Arrays.asList(SPACE.split(x)).iterator();
+            }
+        });
+
+
+        JavaPairDStream<String, Integer> wordCounts = words.mapToPair(
+                new PairFunction<String, String, Integer>() {
+                    @Override
+                    public Tuple2<String, Integer> call(String s) {
+                        return new Tuple2<>(s, 1);
+                    }
+                }).reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer i1, Integer i2) {
+                return i1 + i2;
+            }
+        });
+
+        wordCounts.print();
+        streamingContext.start();
+        streamingContext.awaitTermination();
+
+    }
 }
